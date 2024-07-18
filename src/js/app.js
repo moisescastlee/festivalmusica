766e5932a9c6fbca8b535490a8ed1f5c3511935e
moisescastlee <el_moises_05@hotmail.com>
2024-07-17 20:55:21 -0400

    miercoles

---
+++ src/js/app.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', function() {
+    crearGaleria()
+
+})
+
+function crearGaleria(){
+    const CANTIDAD_IMAGENES = 16
+    const galeria = document.querySelector('.galeria-imagenes')
+
+    for(let i = 1; i <= CANTIDAD_IMAGENES; i++) {
+        const imagen = document.createElement('IMG')
+        imagen.src = `src/img/gallery/full/${i}.jpg`
+        imagen.alt = 'Imagen Galeria'
+
+        imagen.onclick = function() {
+            mostrarImagen(i)
+        }
+        
+        galeria.appendChild(imagen)
+    }
+}
+
+function mostrarImagen(i) {
+    
+    const imagen = document.createElement('IMG')
+    imagen.src = `src/img/gallery/full/${i}.jpg`
+    imagen.alt = 'Imagen Galeria'
+
+    // Generar modal
+    const modal = document.createElement('DIV')
+    modal.classList.add('modal')
+    modal.onclick = cerrarModal
+
+    //Boton cerrar modal
+    const cerrarModalBtn = document.createElement('BUTTON')
+    cerrarModalBtn.textContent = 'X'
+    cerrarModalBtn.classList.add('btn-cerrar')
+    cerrarModalBtn.onclick = cerrarModal
+
+    modal.appendChild(imagen)
+    modal.appendChild(cerrarModalBtn)
+    
+
+    // Agregar al HTML
+    const body = document.querySelector('body')
+    body.classList.add('overflow-hidden')
+    body.appendChild(modal)
+}
+
+function cerrarModal() {
+    const modal = document.querySelector('.modal')
+    modal.classList.add('fade-out')
+
+    setTimeout(() => {
+        modal?.remove()
+        const body = document.querySelector('body')
+        body.classList.remove('overflow-hidden')
+    }, 500);
+}
